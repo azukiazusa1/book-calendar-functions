a40b915c07507f93e1f1790be81ae3577dd04301
azukiazusa1 <cycleippei@gmail.com>
2021-03-23 22:36:43 +0900

    not found

--- functions/src/services/books/findById/index.ts
+++ functions/src/services/books/findById/index.ts
@@ -5,13 +5,17 @@ import { Request } from './types'
 
 export const findById = async (req: Request, context: https.CallableContext) => {
   if (!req.id) {
-    new https.HttpsError('invalid-argument', 'rid is required')
+    new https.HttpsError('invalid-argument', 'id is required')
   }
 
   const id = encodeURIComponent(req.id)
-  const result = await httpClient.get(`/volumes/${id}`)
 
-  return {
-    data: convert(result.data),
+  try {
+    const result = await httpClient.get(`/volumes/${id}`)
+    return {
+      data: convert(result.data),
+    }
+  } catch (e) {
+    new https.HttpsError('not-found', 'not found')
   }
 }
